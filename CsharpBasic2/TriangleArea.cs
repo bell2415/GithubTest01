18678f91b594fe87341076d33b2695879a965c9c
bell2415 <brian3028@naver.com>
2024-06-25 17:21:40 +0900

    Add files via upload

---
+++ CsharpBasic2/TriangleArea.cs
@@ -0,0 +1,46 @@
+using YamlDotNet.Core;
+
+namespace ConsoleApp4
+{
+    internal class TriangleArea
+    {
+        static void Main(string[] args)
+        {
+            // 3 + 5를 변수를 이용하여 결과르르 출력해보세요
+            //int value1 = 3;  // = 대입연산자 R->L
+            //int value2 = 5;
+
+
+            //int result = value1 + value2;
+            //Console.WriteLine(result);
+
+            //---------------------------------------------------------
+            //int value1 = 500;
+            //int value2 = 300;
+
+            //int result = value1 - value2;   
+            //Console.WriteLine(result);
+
+            //---------------------------------------------------------
+            //1. 변수선언 및 입력부
+            //Console.Write("가로값을 입력해 주세요 : ");
+            //int width = int.Parse(Console.ReadLine());
+            //Console.Write("가로값을 입력해 주세요 : ");
+            //int height = int.Parse(Console.ReadLine());
+
+            //2. 알고리즘 수식
+            //int result = width * height; // 수식
+
+            //3. 출력부
+            //Console.WriteLine($"넓이는 {result} 입니다.");
+
+            //---------------------------------------------------------
+
+            //Console.Write("가로값을 입력해 주세요 : ");
+            //int width = int.Parse(Console.ReadLine());
+            //Console.Write("세로값을 입력해 주세요 : ");
+            //int height = int.Parse(Console.ReadLine());
+
+            //double result = width * 0.5 * height;
+
+            //Console.WriteLine($"넓이는 {result} 입니다.");
